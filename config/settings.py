5452b400e8bf6ff95897a40c32892cceefd2df2f
akiyoko <akiyoko@users.noreply.github.com>
2021-02-17 14:34:19 +0900

    django-cors-headers 3.5 対応（変数名変更）

--- config/settings.py
+++ config/settings.py
@@ -131,8 +131,8 @@ SIMPLE_JWT = {
 
 
 # CORS
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
-)
+]
